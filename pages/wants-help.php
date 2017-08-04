057bfceca03c0b65f5a0f7e0d631f46d16c7a7e7
it4joy <drkierkegor@gmail.com>
2017-08-04 14:32:22 +0300

    2017-08-04 :: 14:31 // improve a validation of forms, fix bug with content-type header in base-mail.php

--- pages/wants-help.php
+++ pages/wants-help.php
@@ -10,23 +10,23 @@
 	<meta name="copyright" content="" />
 
 	<title>Благотворительный фонд помощи детям с ДЦП "Али"</title>
-	
+
 	<link rel="shortcut icon" href="favicon.ico">
 	<!-- Add favicons for iphones ! -->
-	
+
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome.min.css" />
 	<link rel="stylesheet" type="text/css" href="js/fancybox/jquery.fancybox.css" /> <!--FancyBox-->
-	
+
 	<!-- Scripts -->
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/fancybox/jquery.fancybox.js"></script>
 	<script type="text/javascript" src="js/fancybox/jquery.mousewheel-3.0.4.js"></script>
-	
+
 	<!--[if lt IE 9]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	
+
 	<!-- <script type="text/javascript">
 		toTop script
 	</script> -->
@@ -35,17 +35,17 @@
 
 <body>
   <div class="page-wrapper">
-  
+
 	<?php require_once('php/header.php'); ?>
-	
+
 	<div class="container">
-		
+
 			<div class="row">
 				<div class="breadcrumbs">
 					<p><a href="index.html">Главная</a><i class="fa fa-chevron-right news-bc-i"></i>Хочу помочь!</p>
 				</div>
 			</div>
-				
+
 				<div class="fullwidth">
 					<h5 class="mbot-h">Хочу помочь!</h5>
 					<!-- Yandex.CashBox Payment Form -->
@@ -53,26 +53,26 @@
 						<div class="one-half fleft">
 							<input type="hidden" name="ShopID" value="28812">
 							<input type="hidden" name="scid" value="18839">
-							
+
 							<label for="CustomerNumber" class="ya-cashbox-field-label">Идентификатор клиента (введите номер сотового телефона):</label><br>
-							<input type="text" name="CustomerNumber" size="43" class="field"> <!-- 'size' and below - delete them?.. -->
-							
+							<input type="text" name="CustomerNumber" size="43" id="CustomerNumber" class="field" placeholder="Пример: 79033779757">
+
 							<label for="Sum" class="ya-cashbox-field-label">Сумма:</label><br>
-							<input type="text" name="Sum" size="43" class="field">
-							
+							<input type="text" name="Sum" size="43" id="Sum" class="field">
+
 							<label for="CustName" class="ya-cashbox-field-label">Ф.И.О.:</label><br>
-							<input type="text" name="CustName" size="43" class="field">
-							
+							<input type="text" name="CustName" size="43" id="CustName" class="field">
+
 							<!-- Адрес доставки:<br>
 							<input type=text name="CustAddr" size="43"> <br><br> -->
-							
+
 							<label for="CustEmail" class="ya-cashbox-field-label">E-mail:</label><br>
-							<input type="text" name="CustEmail" size="43" class="field">
-							
+							<input type="text" name="CustEmail" size="43" id="CustEmail" class="field">
+
 							<!-- Содержание заказа:<br>
 							<textarea rows="10" name="OrderDetails" cols="34"></textarea><br><br> -->
 						</div>
-						
+
 						<div class="one-half fleft">
 							<fieldset>
 								<legend class="ya-cashbox-field-label">Способ оплаты:</legend>
@@ -83,20 +83,25 @@
 								<p class="ya-cashbox-field"><input name="paymentType" value="AB" type="radio">Оплата через Альфа-Клик</p>
 								<p class="ya-cashbox-field"><input name="paymentType" value="SB" type="radio">Оплата через Сбербанк: оплата по SMS или Сбербанк Онлайн</p>
 							</fieldset>
-							<input type="submit" value="Оплатить" class="form-order-btn"><br>
+
+							<div class="fullwidth">
+								<button type="submit" class="form-order-btn"> <i class="fa fa-money"></i> Оплатить</button>
+								<button type="reset" class="form-order-btn" style="margin-left:12px;"> <i class="fa fa-eraser"></i> Очистить</button>
+								<div class="clearfix"></div>
+							</div>
 						</div>
 						<div class="clearfix"></div>
 					</form>
 					<!-- Yandex.CashBox Payment Form -->
 				</div>
-			
+
 	</div> <!-- /.container -->
-	
+
 	<?php require_once('php/needs-form.php'); ?>
-	
+
 	<div class="page-buffer"></div>
   </div>
-	
+
 	<?php require_once('php/footer-scripts.php'); ?>
 
 </body>
--- php/app4help.php
+++ php/app4help.php
@@ -1,14 +1,14 @@
 <?php
-	
-	$mailto = "wf_ali@mail.ru"; /* wf_ali@mail.ru */ /* everywhere777@yandex.ru */ /* pruzhina@mail.ru */
-	/* $subject = "Заявка на помощь с сайта"; */
-	
+
+	$mailto = "wf_ali@mail.ru";
+	//$mailto = "drkierkegor@gmail.com";
+
 	$picture = "";
 	if (!empty($_FILES['upload']['tmp_name'])) {
 		$path = $_FILES['upload']['name'];
 		if (copy($_FILES['upload']['tmp_name'], $path)) $picture = $path;
 	}
-	
+
 	$msg = "<html><body>
 		<p><b>ФИО ребенка:</b> ".$_POST['name_child']."</p>
 		<p><b>Дата рождения:</b> ".$_POST['birthday']."</p>
@@ -21,13 +21,15 @@
 		<p><b>Комментарий отправителя:</b> ".$_POST['msg']."</p>
 		</body></html>";
 	$msg = strip_tags($msg, '<p><a></br><b>');
-	
+
 	// sending letter
 	if(empty($picture)) include('base-mail.php');
 	else send_mail($mailto, $msg, $picture);
-	
+
 	// auxiliary function for sending letter with attachment
 	function send_mail($to, $html, $path) {
+		$thm = "Заявка на помощь с сайта";
+
 		$fp = fopen($path,"r");
 		if (!$fp) {
 			print "Файл $path не может быть прочитан!";
@@ -35,7 +37,7 @@
 		}
 		$file = fread($fp, filesize($path));
 		fclose($fp);
-		
+
 		$boundary = "--".md5(uniqid(time())); // generate a separator
 		$headers .= "MIME-Version: 1.0\n";
 		$headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\n";
@@ -45,20 +47,20 @@
 		$multipart .= "Content-Type: text/html; charset=$kod\n";
 		$multipart .= "Content-Transfer-Encoding: Quot-Printed\n\n";
 		$multipart .= "$html\n\n";
-		
+
 		$message_part = "--$boundary\n";
 		$message_part .= "Content-Type: application/octet-stream\n";
 		$message_part .= "Content-Transfer-Encoding: base64\n";
 		$message_part .= "Content-Disposition: attachment; filename = \"".$path."\"\n\n";
 		$message_part .= chunk_split(base64_encode($file))."\n";
 		$multipart .= $message_part."--$boundary--\n";
-		
+
 		if(!mail($to, $thm, $multipart, $headers)) {
 			header('Refresh: 5; url=/');
 			include('f-msg-error.php');
 		}
 	}
-	// Auto redirect to main page
+
+	// Auto redirect to main page;
 	header('Refresh: 5; url=/');
 	include('f-msg-ok.php');
-?>
