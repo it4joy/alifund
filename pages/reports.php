0a9ab6fe222c0aecb231a96f1a720370dcc93fb2
Igor Ivanov <drkierkegor@gmail.com>
2017-09-18 12:00:33 +0300

    2017-09-18 :: 12:00 // updated materials

--- pages/reports.php
+++ pages/reports.php
@@ -10,23 +10,23 @@
 	<meta name="copyright" content="" />
 
 	<title>Благотворительный фонд помощи детям с ДЦП "Али"</title>
-	
+
 	<link rel="shortcut icon" href="favicon.ico">
 	<!-- Add favicons for iphones ! -->
-	
+
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome.min.css" />
 	<link rel="stylesheet" type="text/css" href="js/fancybox/jquery.fancybox.css" /> <!--FancyBox-->
-	
+
 	<!-- Scripts -->
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/fancybox/jquery.fancybox.js"></script>
 	<script type="text/javascript" src="js/fancybox/jquery.mousewheel-3.0.4.js"></script>
-	
+
 	<!--[if lt IE 9]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	
+
 	<!-- <script type="text/javascript">
 		toTop script
 	</script> -->
@@ -35,30 +35,35 @@
 
 <body>
   <div class="page-wrapper">
-  
+
 	<?php require_once('php/header.php'); ?>
-	
+
 	<div class="container">
-		
+
 			<div class="row">
 				<div class="breadcrumbs">
 					<p><a href="index.html">Главная</a><i class="fa fa-chevron-right news-bc-i"></i>Отчеты</p>
 				</div>
 			</div>
-				
+
 				<div class="fullwidth">
 					<h5 class="mbot-h">Отчеты</h5>
 					<h6 class="mbot20"><i class="fa fa-calendar"></i> 2017</h6>
-					
+
+					<div class="block mbot20">
+						<h6 class="inline"><i class="fa fa-file-text mright10"></i> Отчет о проведении праздника &laquo;Карусель&raquo; для детей с ДЦП и их семей (01.07.)</h6>
+						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/2017/holiday-carousel-03-07/holiday_carousel-2017-07-03.pdf" target="_blanc">Просмотреть / Скачать (.pdf)</a></h6>
+					</div>
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-parents-meeting-30_04" href="docs/reports/2017/parents-club-meeting-30-04/meeting-report-2017-04-30-p1-c.jpg" title="Отчет о проведении встречи с родителями детей с ДЦП в рамках деятельности «Клуба родителей»">
-						Отчет о проведении встречи с родителями детей с ДЦП в рамках деятельности &laquo;Клуба родителей&raquo;</a> (30.04.)</h6>
+						Отчет о проведении встречи с родителями детей с ДЦП в рамках деятельности &laquo;Клуба родителей&raquo;</a> (30.03.)</h6>
 						<a href="docs/reports/2017/parents-club-meeting-30-04/meeting-report-2017-04-30-p2-c.jpg" rel="report-parents-meeting-30_04" class="grouped" title="Отчет о проведении встречи с родителями детей с ДЦП в рамках деятельности «Клуба родителей»"></a>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/2017/parents-club-meeting-30-04/meeting-report-2017-04-30-c_m.pdf" target="_blanc">Скачать</a></h6>
 					</div>
 
 					<h6 class="mbot20"><i class="fa fa-calendar"></i> 2016</h6>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-conference-08_06" href="docs/reports/2016/Report-conference-08_06/page-01.jpg" title="Отчет о проведении I Межрегиональной конференции «Дети с ДЦП: задачи, технологии, перспективы»">
 						Отчет о проведении I Межрегиональной конф. «Дети с ДЦП: задачи, технологии, перспективы»</a> (08.06.)</h6>
@@ -67,14 +72,14 @@
 						<a href="docs/reports/2016/Report-conference-08_06/page-04.jpg" rel="report-conference-08_06" class="grouped" title="Отчет о проведении I Межрегиональной конференции «Дети с ДЦП: задачи, технологии, перспективы»"></a>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/2016/Report-conference-08_06/Report-conference-08_06.pdf" target="_blanc">Скачать</a></h6>
 					</div>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-concert-02_06" href="docs/reports/2016/Report-concert-02_06/page-001.jpg" title="Отчет о проведении Большого благотворительного концерта артистов Чувашии">
 						Отчет о проведении Большого благотворительного концерта артистов Чувашии</a> (02.06.)</h6>
 						<a href="docs/reports/2016/Report-concert-02_06/page-002.jpg" rel="report-concert-02_06" class="grouped" title="Отчет о проведении Большого благотворительного концерта артистов Чувашии"></a>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/2016/Report-concert-02_06/Report-concert-02_06.pdf" target="_blanc">Скачать</a></h6>
 					</div>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-carousel-2016" href="docs/reports/2016/Report-Carousel-holiday-29_05/page-01.jpg" title="Отчет о проведении дня отдыха для детей с ДЦП «Карусель»">
 						Отчет о проведении дня отдыха для детей с ДЦП «Карусель»</a> (29.05.)</h6>
@@ -83,11 +88,11 @@
 						<a href="docs/reports/2016/Report-Carousel-holiday-29_05/page-04.jpg" rel="report-carousel-2016" class="grouped" title="Отчет о проведении дня отдыха для детей с ДЦП «Карусель»"></a>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/2016/Report-Carousel-holiday-29_05/Report-Carousel-holiday-29_05.pdf" target="_blanc">Скачать</a></h6>
 					</div>
-					
+
 					<!-- *** -->
-					
+
 					<h6 class="mbot20"><i class="fa fa-calendar"></i> 2015</h6>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-6" href="docs/reports/Report-NY-2015/report-ny-12-21_12-1.jpg" title="Отчет о проведении новогоднего мероприятия «Новый год приходит в дом»">
 						Отчет о проведении новогоднего мероприятия «Новый год приходит в дом»</a> (12-21.12.)</h6>
@@ -96,7 +101,7 @@
 						<a href="docs/reports/Report-NY-2015/report-ny-12-21_12-4.jpg" rel="report-6" class="grouped"></a>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/Report-NY-2015/report-ny-2015.pdf" target="_blanc">Скачать</a></h6>
 					</div>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-5" href="docs/reports/report-seminar-20_11-1.jpg" title="Отчет о проведении семинара-практикума: У моего ребенка ДЦП. Как быть?">
 						Отчет о проведении тематического семинара-практикума «У моего ребенка ДЦП. Как быть?»</a> (20.11.)</h6>
@@ -105,19 +110,19 @@
 						<a href="docs/reports/report-seminar-20_11-4.jpg" rel="report-5" class="grouped"></a>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/report-seminar-20_11.pdf" target="_blanc">Скачать</a></h6>
 					</div>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-4" href="docs/reports/report-holiday-carousel-31_05_15.jpg" title="Отчет о проведении дня отдыха для детей с ДЦП «Карусель»">
 						Отчет о проведении дня отдыха для детей с ДЦП «Карусель»</a> (31.05.)</h6>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/report-holiday-carousel-31_05_15.pdf" target="_blanc">Скачать</a></h6>
 					</div>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-3" href="docs/reports/report-concert-28_04_15.jpg" title="Отчет о проведении благотворительного концерта артистов чувашской эстрады «С любовью к детям»">
 						Отчет о проведении благотворительного концерта артистов чувашской эстрады «С любовью к детям»</a></h6>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/report-concert-28_04_15.pdf" target="_blanc">Скачать</a></h6>
 					</div>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-2" href="docs/reports/report-seminar-22_04-1.jpg" title="Отчет о проведении семинара-практикума: У моего ребенка ДЦП. Как быть?">
 						Отчет о проведении тематического семинара-практикума «У моего ребенка ДЦП. Как быть?»</a> (22.04.)</h6>
@@ -125,29 +130,29 @@
 						<a href="docs/reports/report-seminar-22_04-3.jpg" rel="report-2" class="grouped"></a>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/report-seminar-22_04.pdf" target="_blanc">Скачать</a></h6>
 					</div>
-					
+
 					<hr>
-					
+
 					<h6 class="mbot20"><i class="fa fa-calendar"></i> 2014</h6>
-					
+
 					<div class="block mbot20">
 						<h6 class="inline"><i class="fa fa-file-text mright10"></i><a class="grouped" rel="report-1" href="docs/reports/Report-NY.jpg" title="Отчет о проведении новогодней акции «Новый год приходит в дом»">
 						Отчет о проведении новогодней акции «Новый год приходит в дом»</a></h6>
 						<h6 class="inline mleft16"><i class="fa fa-download mright10"></i><a href="docs/reports/Report-NY.pdf" target="_blanc">Скачать</a></h6>
 					</div>
 				</div>
-		
+
 			<div class="clearfix"></div>
-			
+
 	</div> <!-- /.container -->
-	
+
 	<?php require_once('php/needs-form.php'); ?>
-	
+
 	<div class="page-buffer"></div>
   </div>
-	
+
 	<?php require_once('php/footer-scripts.php'); ?>
-	
+
 	<script type="text/javascript">
 		jQuery(document).ready(function() {
 			jQuery("a.grouped").fancybox({
